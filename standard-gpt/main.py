bb85663f97d9ca475fcfcbda8aaa4f6241a72358
Beca <matricabytess24@gmail.com>
2025-12-26 09:48:05 +0300

    standard gpt2 training piple ready

--- standard-gpt/main.py
+++ standard-gpt/main.py
@@ -3,10 +3,13 @@ import random
 import torch.nn as nn
 import torch.nn.functional as F
 import torch
-with open(r'C:\Users\beca\Desktop\sfs\ebt\tiny_shakspeare.txt','r',encoding='utf-8') as f: 
+from sklearn.model_selection import train_test_split 
+
+with open(r'tiny_shakespeare.txt','r',encoding='utf-8') as f: 
     text = f.read()
 
 
+torch.set_default_device('cuda')
 
 enc = tiktoken.get_encoding('cl100k_base')
 ids = enc.encode(text)
@@ -19,13 +22,14 @@ class Attention(nn.Module):
         self.query = nn.Linear(embed_size,head_size)
         self.key = nn.Linear(embed_size,head_size)
         self.value = nn.Linear(embed_size,head_size)
+        self.embed_size = embed_size
         
     def forward(self,embds):
         query = self.query(embds)
         key = self.key(embds)
         value = self.value(embds)
 
-        scores = query @ key.transpose(-2,-1) / self.head_size ** 0.5
+        scores = query @ key.transpose(-2,-1) / self.embed_size ** 0.5
         mask = torch.tril(torch.ones_like(scores))
         scores = scores.masked_fill(mask==0,float('-inf'))
         attention_weights = torch.softmax(scores,dim=-1)
@@ -61,7 +65,10 @@ class Block(nn.Module):
         self.ff = nn.Sequential(
             nn.Linear(self.embed_size, 4*self.embed_size),
             nn.GELU(),
-            nn.Linear(self.embed_size*4,self.embed_size)) 
+            nn.Linear(self.embed_size*4,self.embed_size),
+            nn.Dropout(0.2),
+            ) 
+        
 
         self.ln1 = nn.LayerNorm(self.embed_size)
         self.ln2 = nn.LayerNorm(self.embed_size)
@@ -112,51 +119,91 @@ class Transformer(nn.Module):
         best = torch.argmax(last_token)
         return enc.decode([best.tolist()])
 
-    def train(self,train_data: torch.Tensor,epochs=10,seq_len=1024,batch_size=200):
+    def test(self,X_test,y_test,batch_size):
+        total_loss= 0
+        num_batches = 0
+        for j in range(0,len(X_test),batch_size):
+                x = X_test[j: j + batch_size]
+                y = y_test[j: j + batch_size]
+                num_batches +=  1
+                with torch.no_grad(): 
 
-        xs = []
-        ys = []
+                    logits = self.forward(x)
+                    y = y.flatten()
+                    logits = logits.view(-1,logits.shape[-1])
+                    
+                    total_loss += F.cross_entropy(logits,y)
+        loss = total_loss/ num_batches
+        return loss
         
-        optimizer = torch.optim.AdamW(self.parameters(),lr=1e-3,weight_decay=1e-2)
+
+
+    def train(self,train_data: torch.Tensor,epochs=40,seq_len=1024,batch_size=15):
+        train_proportion = 0.8
+        X_train = []
+        y_train = []
+        X_test = []
+        y_test = []
+
+
+        
+        optimizer = torch.optim.AdamW(self.parameters(),lr=1e-2,weight_decay=1e-2)
         for i in range(0,train_data.shape[-1],seq_len):
             x = train_data[i:i+seq_len]
             y = train_data[i+1:i+seq_len+1]
+
             if x.shape[-1]<seq_len: 
                 break 
-            xs.append(x)
-            ys.append(y)
+            if i < train_proportion * train_data.shape[-1]:
+                X_train.append(x)
+                y_train.append(y)
+            else: 
+                X_test.append(x)
+                y_test.append(y)
 
-        xs = torch.stack(xs) 
-        ys = torch.stack(ys)
+        X_train = torch.stack(X_train)
+        y_train = torch.stack(y_train)
+        X_test = torch.stack(X_test)
+        y_test = torch.stack(y_test)
+
         
         for epoch in range(epochs): 
-            print(f'staring epoch : {epoch+1}')
-
-            for i in range(0,len(xs),batch_size):
-                x = xs[i: i + batch_size]
-                y = ys[i: i + batch_size]
+            total_loss = 0 
+            num_batches = 0 
+            for i in range(0,len(X_train),batch_size):
+                num_batches += 1
+                x = X_train[i: i + batch_size]
+                y = y_train[i: i + batch_size]
 
                 logits = self.forward(x)
+                y = y.flatten()
+                logits = logits.view(-1,logits.shape[-1])
+                
                 loss = F.cross_entropy(logits,y)
+                total_loss += loss.item()
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
-
-
-            if (epoch + 1) % 10 ==0 : 
-                    print(f"Epoch : {epoch} , Loss: {loss}")
-
-
-
+            train_loss = total_loss / num_batches
+            test_loss = self.test(X_test,y_test,batch_size)
+            
+            print(f"Epoch : {epoch} , Train Loss: {train_loss}, Test Loss: {test_loss}")
 
 
 
-model  = Transformer(128,8,12,enc.n_vocab,1024)
+        
+
+
+
+
+
+model  = Transformer(128,4,4,enc.n_vocab,1024)
 ids = torch.tensor(ids)
 # print(ids.shape)
-
-
 model.train(ids)
+torch.save(model.state_dict(),'final.pt')
+
+
 
 
 
